15d3c7360ca53ff333e8830c43110401d504574a
Robson Lana <robsonlana105@gmail.com>
2023-08-20 12:14:52 -0300

    Consumer adapted to receive in avro

--- spark/work-dir/consumer.py
+++ spark/work-dir/consumer.py
@@ -1,36 +1,62 @@
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
+from pyspark.sql.avro.functions import from_avro
+from pyspark.sql.types import StructType, StructField, TimestampType, FloatType
 from pyspark.streaming import StreamingContext
 
-def process(message):
-    print('Message Received')
-    print(message)
-    return message
+table_schema = StructType([
+    StructField('ot', TimestampType()),
+    StructField('ct', TimestampType()),
+    StructField('o', FloatType()),
+    StructField('h', FloatType()),
+    StructField('l', FloatType()),
+    StructField('c', FloatType()),
+    StructField('v', FloatType()),
+])
+
+avro_schema = """
+{
+    "type": "record",
+    "name": "kandles",
+    "fields": [
+        { "name": "ot", "type": { "type": "long", "logicalType": "timestamp-millis" } },
+        { "name": "ct", "type": { "type": "long", "logicalType": "timestamp-millis" } },
+        { "name": "o", "type": "float" },
+        { "name": "h", "type": "float" },
+        { "name": "l", "type": "float" },
+        { "name": "c", "type": "float" },
+        { "name": "v", "type": "float" }
+    ]
+}
+"""
+
+kafka_params = {
+    "kafka.bootstrap.servers": "kafka:9092",
+    "group.id": "k-connect-cluster",
+    "subscribe": "bitcoin_chart_topic",
+    "kafkaConsumer.pollTimeoutsMs": '5000',
+    "startingOffsets": "earliest"
+}
 
 if __name__ == "__main__":
-    kafka_params = {
-        "kafka.bootstrap.servers": "kafka:9092",
-        "group.id": "k-connect-cluster",
-        "subscribe": "bitcoin_chart_topic",
-        "kafkaConsumer.pollTimeoutsMs": '5000'
-    }
 
-
-    spark = SparkSession.builder.appName("spark_kafka_consumer")\
+    spark = SparkSession.builder\
+        .appName("spark_kafka_consumer")\
         .getOrCreate()
 
-    try:
-        df = spark.readStream.format('kafka').options(**kafka_params).load()
-        print(df)
-    except Exception as e:
-        print(e)
-        raise e
+    df = spark.readStream\
+        .format('kafka')\
+        .options(**kafka_params).load()
 
-    processed = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")\
-        .select(col('key').cast('string'), col('value').cast('string'))
+    price_kandle = df.select(from_avro(col("value"), avro_schema).alias("data"))\
+        .select('data.*')
 
-    query = processed.writeStream\
-        .outputMode('append').format('console').start()
+    price_kandle.writeStream\
+        .format("console")\
+        .outputMode("append")\
+        .option("truncate", False)\
+        .start()\
+        .awaitTermination()
 
-    query.awaitTermination()
+    print('Loaded Streaming')
